7a84a90212dd011172e5b1818c721f517671712f
Tarcisio R Sacramento <tarcisacra333@gmail.com>
2022-08-29 10:36:38 -0400

    Requisito 2 :) Devagar Sem Pressa!

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return data.species
+    .find((element) => element.name === animal).residents
+    .every((element) => element.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
