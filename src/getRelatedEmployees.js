0739bfebc079c26b4e221107ce891e309e91850d
Tarcisio R Sacramento <tarcisacra333@gmail.com>
2022-08-31 19:12:43 -0400

    Requisito 5

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,22 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const manTuE = data.employees.find((algo) => algo.id === id);
+  if (manTuE.managers.length < 2) {
+    return true;
+  }
+  return false;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return data.employees.filter((elemento) => elemento.managers.includes(managerId))
+    .map((elemento) => `${elemento.firstName} ${elemento.lastName}`);
+
 }
 
+console.log(getRelatedEmployees('fdb2543b-5662-46a7-badc-93d960fdc0a8'));
 module.exports = { isManager, getRelatedEmployees };
