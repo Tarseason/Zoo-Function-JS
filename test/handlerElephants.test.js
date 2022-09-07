b0cac0625092d9d5d076095c55ce2d83e28f32e0
Tarcisio R Sacramento <tarcisacra333@gmail.com>
2022-09-07 15:32:48 -0400

    requisito 6 Stmts 87.5

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,22 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('testa quantia de animais', () => {
+    expect(handlerElephants('count')).toEqual(4);
+  });
+  it('Testa o retorno de um array de nomes', () => {
+    expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
+  });
+  it('Testa a media da idade dos elefantes', () => {
+    expect(handlerElephants('averageAge')).toEqual(10.5);
+  });
+  it('Testa o retorno da localização dos elefantes', () => {
+    expect(handlerElephants('location')).toEqual('NW');
+  });
+  it('Testa a popularidade dos elefantes', () => {
+    expect(handlerElephants('popularity')).toEqual(5);
+  });
+  it('Testa o retorno de um array de dias possiveis para visita aos elefantes', () => {
+    expect(handlerElephants('availability')).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
+  });
+});
