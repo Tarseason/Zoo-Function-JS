89f64c167c969177d6c7065d95b6ab673fa48754
Tarcisio R Sacramento <tarcisacra333@gmail.com>
2022-09-07 16:28:52 -0400

    requisito 6 stmts 100 finalizado

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -10,8 +10,15 @@ describe('Testes da função HandlerElephants', () => {
   it('Testa a media da idade dos elefantes', () => {
     expect(handlerElephants('averageAge')).toEqual(10.5);
   });
+  it('Testa se o parametro da funcao computeData esta incorreto', () => {
+    expect(handlerElephants('')).toEqual(null);
+  });
   it('Testa o retorno da localização dos elefantes', () => {
     expect(handlerElephants('location')).toEqual('NW');
+  });
+  it('Testa entrada divergente da funcao handlerElephants', () => {
+    expect(handlerElephants(undefined)).toBeUndefined();
+    expect(handlerElephants(2)).toBe('Parâmetro inválido, é necessário uma string');
   });
   it('Testa a popularidade dos elefantes', () => {
     expect(handlerElephants('popularity')).toEqual(5);
