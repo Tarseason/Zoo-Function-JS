00d20a270678a38fd04bc90b4f74a859f87b6c52
Tarcisio R Sacramento <tarcisacra333@gmail.com>
2022-09-07 11:47:29 -0400

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return data.species.reduce((acc, animalia) => {
+      acc[animalia.name] = animalia.residents.length;
+      console.log(acc);
+      return acc;
+    }, {});
+  }
+  if (animal.specie && animal.sex) {
+    return data.species.find((elemento) => elemento.name === animal.specie).residents
+      .filter((element) => element.sex === animal.sex).length;
+  }
+  return data.species.find((elemento) => elemento.name === animal.specie).residents.length;
 }
-
+// referencia do reduce a seguir
+// https://www.youtube.com/watch?v=tU5EOsJN0w4
 module.exports = countAnimals;
