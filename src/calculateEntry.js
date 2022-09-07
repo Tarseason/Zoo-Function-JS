f581bbc9056d1e47780bb2d94efff7f7d4291f39
Tarcisio R Sacramento <tarcisacra333@gmail.com>
2022-09-07 17:52:25 -0400

    requisito 8 primeira funcao feita, pularei para outra

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,27 @@
 const data = require('../data/zoo_data');
 
+// const entrant = [
+// 	{ name:  'Lara Carvalho', age:  5 },
+// 	{ name:  'Frederico Moreira', age:  5 },
+// 	{ name:  'Pedro Henrique Carvalho', age:  5 },
+// 	{ name:  'Maria Costa', age:  18 },
+// 	{ name:  'Núbia Souza', age:  18 },
+// 	{ name:  'Carlos Nogueira', age:  50 },
+// ];
+
 function countEntrants(entrants) {
-  // seu código aqui
-  // meu codigo aqui
+  const visitantes = {
+    child: entrants.filter((elemento) => elemento.age < 18).length,
+    adult: entrants.filter((elemento) => elemento.age >= 18 && elemento.age < 50).length,
+    senior: entrants.filter((elemento) => elemento.age >= 50).length,
+  };
+  return visitantes;
 }
 
 function calculateEntry(entrants) {
+  if (entrants === {}) {
+    return entrants.filter((elemento) => elemento).length;
+  }
   // seu código aqui
 }
 
