1b991d194af19305276cf79ea7dbde039b9c8c44
Tarcisio R Sacramento <tarcisacra333@gmail.com>
2022-09-09 10:33:32 -0400

    requisito 8, apaga linhas comentadas

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,15 +1,6 @@
 const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-// const entrant = [
-// 	{ name:  'Lara Carvalho', age:  5 },
-// 	{ name:  'Frederico Moreira', age:  5 },
-// 	{ name:  'Pedro Henrique Carvalho', age:  5 },
-// 	{ name:  'Maria Costa', age:  18 },
-// 	{ name:  'Núbia Souza', age:  18 },
-// 	{ name:  'Carlos Nogueira', age:  50 },
-// ];
-
 function countEntrants(entrants) {
   const visitantes = {
     child: entrants.filter((elemento) => elemento.age < 18).length,
