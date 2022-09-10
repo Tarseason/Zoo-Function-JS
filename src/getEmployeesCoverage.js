68977d6f3ecc83077b09e1c58f4a8fa38de87b94
Tarcisio R Sacramento <tarcisacra333@gmail.com>
2022-09-10 00:37:31 -0400

    requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,44 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { species, employees } = data;
+
+const algo = (abc) => species.filter(({ id }) => abc.includes(id)).map((elemento) => elemento.name);
+
+const alg = (loc) => species.filter(({ id }) => loc.includes(id))
+  .map((elemento) => elemento.location);
+console.log(algo('0938aa23-f153-4937-9f88-4858b24d6bce'));
+
+const retornaAll = () => {
+  const nomea = employees.map(({ id, firstName, lastName, responsibleFor }) => {
+    const obj = {
+      id,
+      fullName: `${firstName} ${lastName}`,
+      species: algo(responsibleFor),
+      locations: alg(responsibleFor),
+    };
+    return obj;
+  });
+  return nomea;
+};
+
+function getEmployeesCoverage(entrada) {
+  if (!entrada) {
+    return retornaAll();
+  }
+  const entradaMesmo = employees
+    .find(({ id, firstName, lastName }) => (firstName === entrada.name
+      || lastName === entrada.name) || (id === entrada.id));
+
+  if (!entradaMesmo) throw new Error('Informações inválidas');
+
+  const objSaida = {
+    id: entradaMesmo.id,
+    fullName: `${entradaMesmo.firstName} ${entradaMesmo.lastName}`,
+    species: algo(entradaMesmo.responsibleFor),
+    locations: alg(entradaMesmo.responsibleFor),
+  };
+  return objSaida;
 }
 
+// console.log(getEmployeesCoverage());
 module.exports = getEmployeesCoverage;
