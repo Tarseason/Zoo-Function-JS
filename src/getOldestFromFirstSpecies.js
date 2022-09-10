6e49f42ef8ef005b6360c91f725b95ed484ad97d
Tarcisio R Sacramento <tarcisacra333@gmail.com>
2022-09-09 22:50:15 -0400

    requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const a = data.employees.find((elemento) => elemento.id === id).responsibleFor[0];
+  const b = data.species.find((elemento) => elemento.id === a).residents
+    .sort((age1, age2) => age2.age - age1.age)[0];
+  return Object.values(b);
 }
 
 module.exports = getOldestFromFirstSpecies;
